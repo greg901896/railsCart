4ea94cbc9a1dadd7c8612b6a116dadcee2fbed35
greg901896 <greg0912412472@gmail.com>
2025-02-27 23:32:33 +0800

    暫定訂單

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,18 @@
 class OrdersController < ApplicationController
-
+    before_action :find_order ,:index
     def index
         @check = Checkout.where("user_id = ?",current_user.id)
+        @new_check = OrderItem.where(order_id: params[:id])
     end
+    
+    def edit
+        @new_check = OrderItem.where(order_id: params[:id])
+    end
+
+    def find_order
+        @order = OrderItem.find_by(id: params[:id])
+       
+    end
+
+
 end
