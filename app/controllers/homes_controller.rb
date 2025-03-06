f74fdde239aec648c379198de466314f7d2260fd
greg901896 <greg0912412472@gmail.com>
2025-03-06 12:03:24 +0800

    gre

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,19 +8,19 @@ class HomesController < ApplicationController
 
     def t1
         session[:lan] = "?locale=en"
-        redirect_to homes_path(locale: 'en')
+        redirect_to products_path(locale: 'en')
         
     end
 
     def t2
         session[:lan] = "?locale=zh-TW"
-        redirect_to homes_path(locale: 'zh-TW')
+        redirect_to products_path(locale: 'zh-TW')
         @loc = session[:lan]
     end
 
     def t3
       session[:lan] = "?locale=ru"
-      redirect_to homes_path(locale: 'ru')
+      redirect_to products_path(locale: 'ru')
       @loc = session[:lan]
   end
 end
